ad73aee5a3eb9f5506ffa6932bd08b70567ebbb9
mariomelchor <mario@mariomelchor.com>
2018-01-05 14:10:06 -0800

    added index as data attribute to elements

--- js/scripts.js
+++ js/scripts.js
@@ -6,37 +6,40 @@ var todoData = localStorage.getItem('todoStorage') ? JSON.parse(localStorage.get
 // functions
 var loopTodo = function () {
   todoList.innerHTML = ''
+  todoData.forEach(createItem)
+}
 
-  todoData.forEach(element => {
-    var li = document.createElement('li')
-    var anchor = document.createElement('a')
-    var trashIcon = document.createElement('i')
-    var checkbox = document.createElement('input')
-    var label = document.createElement('label')
+var createItem = function (element, index, array) {
+  var li = document.createElement('li')
+  var anchor = document.createElement('a')
+  var trashIcon = document.createElement('i')
+  var checkbox = document.createElement('input')
+  var label = document.createElement('label')
 
-    label.setAttribute('for', 'checkbox')
-    label.innerText = element
+  label.setAttribute('for', 'checkbox-' + index)
+  label.innerText = element
 
-    checkbox.setAttribute('type', 'checkbox')
-    checkbox.setAttribute('id', 'checkbox')
-    checkbox.setAttribute('name', 'checkbox')
+  checkbox.setAttribute('type', 'checkbox')
+  checkbox.setAttribute('id', 'checkbox-' + index)
+  checkbox.setAttribute('name', 'checkbox')
 
-    trashIcon.classList.add('material-icons')
-    trashIcon.textContent = 'delete'
+  trashIcon.classList.add('material-icons')
+  trashIcon.textContent = 'delete'
 
-    anchor.appendChild(trashIcon)
-    anchor.classList.add('right')
-    anchor.setAttribute('href', '#')
+  anchor.appendChild(trashIcon)
+  anchor.classList.add('right')
+  anchor.setAttribute('href', '#')
+  anchor.setAttribute('data-id', index)
 
-    li.classList.add('collection-item')
-    li.appendChild(checkbox)
-    li.appendChild(label)
-    li.appendChild(anchor)
+  li.classList.add('collection-item')
+  li.setAttribute('data-id', index)
+  li.appendChild(checkbox)
+  li.appendChild(label)
+  li.appendChild(anchor)
 
-    // Add new todo to dom
+  // Add new todo to dom
 
-    todoList.appendChild(li)
-  })
+  todoList.appendChild(li)
 }
 
 loopTodo()

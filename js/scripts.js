91239ad83824f8071f16fb60d0a938be2046b5bf
mariomelchor <mario@mariomelchor.com>
2018-01-05 11:59:16 -0800

    Added checkbox, trash icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,60 @@
 // form
-var form = document.getElementById('form');
-var todoList = document.getElementById('todo-list');
-var todoData = localStorage.getItem('todoStorage') ? JSON.parse(localStorage.getItem('todoStorage')) : [];
+var form = document.getElementById('form')
+var todoList = document.getElementById('todo-list')
+var todoData = localStorage.getItem('todoStorage') ? JSON.parse(localStorage.getItem('todoStorage')) : []
 
 // functions
-var loopTodo = function() {
-  todoList.innerHTML = "";
+var loopTodo = function () {
+  todoList.innerHTML = ''
 
   todoData.forEach(element => {
-    var li = document.createElement('li');
-    li.classList.add('collection-item');
-    li.appendChild(document.createTextNode(element));
+    var li = document.createElement('li')
+    var anchor = document.createElement('a')
+    var trashIcon = document.createElement('i')
+    var checkbox = document.createElement('input')
+    var label = document.createElement('label')
+
+    label.setAttribute('for', 'checkbox')
+    label.innerText = element
+
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.setAttribute('id', 'checkbox')
+    checkbox.setAttribute('name', 'checkbox')
+
+    trashIcon.classList.add('material-icons')
+    trashIcon.textContent = 'delete'
+
+    anchor.appendChild(trashIcon)
+    anchor.classList.add('right')
+    anchor.setAttribute('href', '#')
+
+    li.classList.add('collection-item')
+    li.appendChild(checkbox)
+    li.appendChild(label)
+    li.appendChild(anchor)
 
     // Add new todo to dom
+
     todoList.appendChild(li)
-  });
+  })
 }
-  
-loopTodo();
+
+loopTodo()
 
 // event listener when submitting form
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
 
   // Get value from input
-  var todo = document.getElementById('todo-item');
-  todoData.push(todo.value);
+  var todo = document.getElementById('todo-item')
+  todoData.push(todo.value)
 
   // Save data to the current local store
-  localStorage.setItem('todoStorage', JSON.stringify(todoData));
+  localStorage.setItem('todoStorage', JSON.stringify(todoData))
 
   // Append li
-  loopTodo();
+  loopTodo()
 
   // Clear input field
-  todo.value = '';
+  todo.value = ''
 })
